555d537090fa7a5ea74940f1ec834af44ad80c32
lyylemon <34963251+lyylemon@users.noreply.github.com>
2017-12-30 23:47:20 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+App({
+
+  onLaunch() {
+
+    console.log('App.onLaunch()');
+
+  },
+
+  onShow: function () {
+
+  },
+
+  onHide: function () {
+
+  },
+
+
+
+  //本应用全局数据
+
+  globalData: {
+button:{}
+  }
+
+})
